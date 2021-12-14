50979408983ab09f6081c90e19dc7b206d072798
Francisca <fraan928@gmail.com>
2021-12-14 00:45:25 -0300

    deploy heroku

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 
-import questionRouter from './routers/questionsRouter';
-import studentRouter from './routers/studentsRouter';
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/questions', questionRouter);
-app.use('/users', studentRouter);
+app.get('/', () => {
+    console.log('test');
+});
 
 export {
     app,
